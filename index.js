36e5819f7fa68cf8275c9ea66dc4fe8b856bf0e0
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-18 22:37:36 +0000

    enabled port to be set by environment variable. default port is set to 8080

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = (process.env.PORT || 8080); // default port 8080
+
+app.set("view engine", "ejs")
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
