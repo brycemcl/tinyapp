30d60fd963fb94bd89c88e37277a895b9cf6eb27
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-19 02:57:09 +0000

    added logic and path for redirecting based off of token at path /urls/token

--- index.js
+++ index.js
@@ -10,18 +10,36 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  const templateVars = {};
-  res.render("index", templateVars);
+  const templateVars = {
+    title: "",
+    body: "../body",
+    head: "empty",
+  };
+  res.render("partials/shell", templateVars);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    title: "test",
+    title: "URLs",
     urls: urlDatabase,
     body: "../urls/body",
+    head: "empty",
   };
   res.render("partials/shell", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    title: req.params.shortURL,
+    urls: urlDatabase,
+    body: "../urls/body",
+    head: "../urls/redirect/redirectHead",
+    shortURL: req.params.shortURL
+  };
+  res.render("partials/shell", templateVars);
+  console.log(templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
